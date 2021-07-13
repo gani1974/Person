31aa99686f61598d52ad17dcc9ba213f5a3a47f8
Gani1974 <ganiiz@mail.ru>
2021-07-13 09:48:48 +0600

    Test

--- src/Errors/Main.java
+++ src/Errors/Main.java
@@ -1,9 +1,5 @@
 package Errors;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-
 public class Main {
     public static void main(String[] args) {
         //Create first car
