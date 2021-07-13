d666101af65084d7557ce773a3c5ab4cbdf736ed
Gani1974 <ganiiz@mail.ru>
2021-07-13 13:02:21 +0600

    Test 1

--- src/Errors/Main.java
+++ src/Errors/Main.java
@@ -12,6 +12,12 @@ public class Main {
                 .withColor("Green")
                 .withEngine(new Engine("Gaz engine"))
                 .build();
+        //Create third car
+        Car car3 = new Car.Builder()
+                .withName("Honda")
+                .withColor("Yellow")
+                .withEngine(new Engine("Dizel engine"))
+                .build();
 
         System.out.println(car1.toString());
         car1.setColor("Black");
